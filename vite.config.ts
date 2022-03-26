994c34f567fd5af6c22425d0bbd29247feacd544
Gemone <git@gemo.one>
2022-03-26 19:08:31 +0800

    update vite config

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+import { resolve } from 'path';
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vite-plugin-mock-lite',
+      fileName: (format) => `vite-plugin-mock.lite.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['fs'],
+    },
+  },
+});
